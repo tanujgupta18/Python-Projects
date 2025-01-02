68b8d69575fa76b90435449811dcddfeeee255ad
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2025-01-02 17:00:08 +0530

    Update crackpdf.py

--- PDF Password Cracker/crackpdf.py
+++ PDF Password Cracker/crackpdf.py
@@ -1,26 +1,33 @@
 import PyPDF2
 import itertools
+import time
 
-def brute_force_pdf_password(file_path, max_length):
-    # charset = ' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*+-= '
-    charset = '0123456789'
+def brute_force_pdf_password(file_path, max_length, charset):
     with open(file_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
 
+        if not reader.is_encrypted:
+            print("The PDF is not encrypted!")
+            return None
+
+        start_time = time.time()
         for length in range(1, max_length + 1):
             for attempt in itertools.product(charset, repeat=length):
                 password = ''.join(attempt)
                 try:
+                    print(f"Trying password: {password}")
                     if reader.decrypt(password):
                         print(f"Success! The Password is: {password}")
+                        print(f"Time taken: {time.time() - start_time} seconds")
                         return password
                 except Exception as e:
-                    print(f"An error occurred: {e}")
                     continue
 
-    print("Sorry! Password not found")
-    return None
+        print("Password not found.")
+        return None
 
-file_path = 'Bank.pdf'
-max_length = 11
-brute_force_pdf_password(file_path, max_length)
+file_path = 'Test.pdf'
+max_length = 6
+charset = '0123456789'
+
+brute_force_pdf_password(file_path, max_length, charset)
